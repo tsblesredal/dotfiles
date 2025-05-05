083f1b77c721b8009003b9e9739414c0d501d920
tsblesredal <b1508vms@gmail.com>
2025-05-05 17:47:57 +0900

    nvim/行番号の色を変更

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -11,11 +11,15 @@ return {
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Comment", { fg = "#9ece6a", italic = true })
 
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#19dfcf", bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#19dfcf", bg = "none" })
     vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
     vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
     vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "none", fg = "#19dfcf" })
-    TelescopeNormal = { fg = "#ebfafa", bg = "none" }
+    -- Telescopeの透明化設定
+    -- TelescopeNormal = { fg = "#ebfafa", bg = "none" }
   end
 }
